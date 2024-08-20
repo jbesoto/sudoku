20b8b4db39f3b794ddd9c8ac64f0bc7d94c4e99d
Juan Diego Becerra <jdb9056@nyu.edu>
2024-08-20 00:17:26 -0500

    func: commit PrintBoard function
    Implemented a function that prints the board given a pointer to the 2D array.
    This is needed for debugging purposes.

--- include/sudoku.h
+++ include/sudoku.h
@@ -1,6 +1,10 @@
 #ifndef SUDOKU_H_
 #define SUDOKU_H_
 
+#define ROWS 9
+#define COLS 9
+
+void PrintBoard(int board[ROWS][COLS]);
 void PrintHelp(void);
 
 #endif  // SUDOKU_H_
--- src/sudoku.c
+++ src/sudoku.c
@@ -12,6 +12,33 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
+void PrintBoard(int board[ROWS][COLS]) {
+  if (!board) {
+    fprintf(stderr, "Error: board is NULL\n");
+    return;
+  }
+
+  printf("#---#---#---#---#---#---#---#---#---#\n");
+  for (size_t i = 0; i < ROWS; i++) {
+    for (size_t j = 0; j < COLS; j++) {
+      if (board[i][j] == 0) {
+        printf("|   ");
+      } else {
+        printf("| %d ", board[i][j]);
+      }
+
+      if (j + 1 == COLS) {
+        printf("|\n");
+      }
+    }
+    if (i > 0 && (i + 1) % 3 == 0) {
+      printf("#---#---#---#---#---#---#---#---#---#\n");
+    } else {
+      printf("#-----------#-----------#-----------#\n");
+    }
+  }
+}
+
 void PrintHelp(void) {
   printf("sudoku - A program that solves 9x9 sudoku puzzles.\n");
   printf("\n");
